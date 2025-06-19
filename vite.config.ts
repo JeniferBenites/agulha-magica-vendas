9d035d60371f85dc669e58e6edca5becc00c968e
JeniferBenites <jeni.benites69@gmail.com>
2025-06-19 19:55:26 -0300

    troquei o caminho do github

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: "/https://github.com/JeniferBenites/agulha-magica-vendas.git/",
+  base: "/agulha-magica-vendas/",
   plugins: [
     react(),
     mode === 'development' &&
